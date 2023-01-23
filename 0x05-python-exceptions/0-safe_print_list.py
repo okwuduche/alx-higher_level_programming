46b6dad2f8debc84e90648e3ef8ecb07e4a70dbb
okwuduche <chinweikeokwuduche@gmail.com>
2023-01-23 02:38:26 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def safe_print_list(my_list=[], x=0):
     i = 0
     printed = 0
@@ -9,6 +8,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             printed += 1
         except:
-            print("Failed")
+            continue
     print()
     return worked
