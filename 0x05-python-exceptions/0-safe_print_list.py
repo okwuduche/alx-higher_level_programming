18e464a2234f29275664d472f0c3c46e8ea7a09e
okwuduche <chinweikeokwuduche@gmail.com>
2023-01-23 02:49:14 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-        return True
-    except:
-        return False
+
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    printed = 0
+    for i in range(0, x):
+        try:
+            print("{}".format(my_list[i]))
+            printed += 1
+        except:
+            continue
+    print()
+    return printed
